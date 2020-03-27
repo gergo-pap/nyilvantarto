ad7499578b3fa683f429842510ca9089a76b43de
Pap Gergő <p.gergo99@gmail.com>
2020-03-27 18:26:50 +0100

    Első indításnál automatikus mappák és SQL táblák létrehozása. Elérési út tallózható ha nincs az adatbázisban - Még nincs befolyással a formokra.

--- Nyilvantarto_v2/FormMain.cs
+++ Nyilvantarto_v2/FormMain.cs
@@ -7,16 +7,170 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace Nyilvantarto_v2
 {
     public partial class FormMain : Form
     {
+
+        MySqlConnection conn;
+        MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand();
+        private static readonly char[] SpecialChars = "!@#$%^&*()".ToCharArray();
+
         public FormMain()
         {
             InitializeComponent();
+            conn = new MySqlConnection("Server=localhost;Database=nyilvantartas;Uid=root;Pwd=");
+            conn.Open();
+            createTables();
+            createDirectiories();
+            getPath();
         }
 
+        public void getPath()
+        {
+
+            string query = "SELECT s_value FROM settings WHERE s_var = 'eleresiUt'; ";
+            var command = conn.CreateCommand();
+            command.CommandText = query;
+            var result = command.ExecuteScalar();
+            if (result == null)
+            {
+                groupBoxEleresi.Visible = true;
+            }
+            else
+            {
+                var path = result.ToString();
+                labelMentesiHely.Text = path;
+                groupBoxEleresi.Visible = false;
+                groupBoxButtons.Visible = true;
+            }
+
+        }
+
+        private void createDirectiories()
+        {
+            Directory.CreateDirectory(@"c:\Users\Pap Gergő\Desktop\Teszt\Középiskola\Anyakönyv\");
+            Directory.CreateDirectory(@"c:\Users\Pap Gergő\Desktop\Teszt\Szakmai Vizsga\Anyakönyv\");
+            Directory.CreateDirectory(@"c:\Users\Pap Gergő\Desktop\Teszt\Szakmai Vizsga\Törzslap\");
+            Directory.CreateDirectory(@"c:\Users\Pap Gergő\Desktop\Teszt\Érettségi\Törzslap\");
+            Directory.CreateDirectory(@"c:\Users\Pap Gergő\Desktop\Teszt\Érettségi\Tanusítvány\");
+
+        }
+
+        private void setPath()
+        {
+            try
+            {
+                int indexOf = labelMentesiHely.Text.IndexOfAny(SpecialChars);
+                if (indexOf == -1)
+                {
+                    string SQL = "INSERT INTO " +
+                        "settings " +
+                        "VALUES" +
+                        "(" +
+                            "NULL, " +
+                            "@s_var, " +
+                            "@s_value " +
+                        ")";
+
+                    cmd.Connection = conn;
+                    cmd.CommandText = SQL;
+                    cmd.Parameters.AddWithValue("@s_var", "eleresiUt");
+                    cmd.Parameters.AddWithValue("@s_value", labelMentesiHely.Text);
+
+                    cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("A fájl neve érvénytelen karaktereket tartalmaz! !@#$%^&*");
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show("SQL hiba " + ex.Number + " has occurred: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            groupBoxEleresi.Visible = false;
+            groupBoxButtons.Visible = true;
+        }
+
+        private void createTables()
+        {
+            var command = conn.CreateCommand();
+            command.CommandText = @"
+                                CREATE TABLE IF NOT EXISTS 
+                                    settings 
+                                    (
+                                    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                    s_var TEXT NOT NULL,
+                                    s_value TEXT NOT NULL
+                                    );
+                                    ";
+            command.ExecuteNonQuery();
+
+            command = conn.CreateCommand();
+            command.CommandText = @"
+                                CREATE TABLE IF NOT EXISTS 
+                                    kozepiskolaanyakonyv 
+                                    (
+                                    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                    ka_tanuloNeve TEXT NOT NULL,
+                                    ka_AnyjaNeve TEXT NOT NULL,
+                                    ka_szerzo TEXT NOT NULL,
+                                    ka_erettsegiEvKezdet INT NOT NULL,
+                                    ka_erettsegiEvVeg INT NOT NULL,
+                                    ka_DokumentumNev TEXT NOT NULL,
+                                    ka_DokLegutobbModositva DATETIME NOT NULL,
+                                    ka_FeltoltesIdopontja DATETIME NOT NULL,
+                                    ka_formatum TEXT NOT NULL,
+                                    ka_path TEXT NULL
+                                    );
+                                    ";
+            command.ExecuteNonQuery();
+
+            command = conn.CreateCommand();
+            command.CommandText = @"
+                                CREATE TABLE IF NOT EXISTS 
+                                    szakmaiviszgaanyakonyv 
+                                    (
+                                    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                    szva_tanuloNeve TEXT NOT NULL,
+                                    szva_AnyjaNeve TEXT NOT NULL,
+                                    szva_szerzo TEXT NOT NULL,
+                                    szva_erettsegiEvKezdet INT NOT NULL,
+                                    szva_erettsegiEvVeg INT NOT NULL,
+                                    szva_DokumentumNev TEXT NOT NULL,
+                                    szva_DokLegutobbModositva DATETIME NOT NULL,
+                                    szva_FeltoltesIdopontja DATETIME NOT NULL,
+                                    szva_formatum TEXT NOT NULL,
+                                    szva_path TEXT NULL
+                                    );
+                                    ";
+            command.ExecuteNonQuery();
+
+            command = conn.CreateCommand();
+            command.CommandText = @"
+                                CREATE TABLE IF NOT EXISTS 
+                                    szakmaivizsgaTorzslap 
+                                    (
+                                    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                    szvt_tanuloNeve TEXT NOT NULL,
+                                    szvt_AnyjaNeve TEXT NOT NULL,
+                                    szvt_szerzo TEXT NOT NULL,
+                                    szvt_viszgaEvKezdet INT NOT NULL,
+                                    szvt_viszgaTavasz1Osz0 BOOLEAN NOT NULL,
+                                    szvt_dokumentumNev TEXT NOT NULL,
+                                    szvt_dokLegutobbModositva DATETIME NOT NULL,
+                                    szvt_feltoltesIdopontja DATETIME NOT NULL,
+                                    szvt_formatum TEXT NOT NULL,
+                                    szvt_path TEXT NULL
+                                    );
+                                    ";
+            command.ExecuteNonQuery();
+        }
         private void buttonErettsegi_Click(object sender, EventArgs e)
         {
             (new FormErettsegi()).Show(); this.Hide();
@@ -32,5 +186,23 @@ namespace Nyilvantarto_v2
         {
             (new FormKozepiskolaAnyakonyv()).Show(); this.Hide();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string sSelectedPath = fbd.SelectedPath;
+                labelMentesiHely.Text = sSelectedPath;
+            }
+            else
+            {
+                MessageBox.Show("Hiba");
+            }
+            setPath();
+        }
+
+        
     }
 }
